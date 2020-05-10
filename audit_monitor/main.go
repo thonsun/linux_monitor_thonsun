ec6cdefbd4d7278f9d63e05c4ec4acd646108e7e
tangsheng.thonsun <tangsheng.thonsun@bytedance.com>
2020-05-10 10:26:22 +0800

    new rule
    Change-Id: I97ac7fdc2bac9c1962ec5eea466b6a1cd6965dcb

--- audit_monitor/main.go
+++ audit_monitor/main.go
@@ -88,20 +88,21 @@ func receive(r *libaudit.AuditClient) error {
 		// 接收kernel audit的消息类型：systemcall
 		switch rawEvent.Type {
 		case auparse.AUDIT_SYSCALL:
-			fmt.Printf("type=%#v %#s","SYSTEM CALL",string(rawEvent.Data))
+			fmt.Printf("type=%#v %#s\n","SYSTEM CALL",string(rawEvent.Data))
 		case auparse.AUDIT_PATH:
-			fmt.Printf("type=%#v %#s","PATH",string(rawEvent.Data))
+			fmt.Printf("type=%#v %#s\n","PATH",string(rawEvent.Data))
 		case auparse.AUDIT_SOCKETCALL:
-			fmt.Printf("type=%#v %#s","SYSTEM SOCKET",string(rawEvent.Data))
+			fmt.Printf("type=%#v %#s\n","SYSTEM SOCKET",string(rawEvent.Data))
 		case auparse.AUDIT_CONFIG_CHANGE:
-			fmt.Printf("type=%#v %#s","SYSTEM CONFIG CHANGE",string(rawEvent.Data))
+			fmt.Printf("type=%#v %#s\n","SYSTEM CONFIG CHANGE",string(rawEvent.Data))
 		case auparse.AUDIT_CWD:
-			fmt.Printf("type=%#v %#s","CWD",string(rawEvent.Data))
+			fmt.Printf("type=%#v %#s\n","CWD",string(rawEvent.Data))
 		case auparse.AUDIT_EXECVE:
-			fmt.Printf("type=%#v %#s","EXECUTE",string(rawEvent.Data))
+			fmt.Printf("type=%#v %#s\n","EXECUTE",string(rawEvent.Data))
 		case auparse.AUDIT_KERNEL_OTHER:
-			fmt.Printf("type=%#v %#s","KERNEL_OTHER",string(rawEvent.Data))
+			fmt.Printf("type=%#v %#s\n","KERNEL_OTHER",string(rawEvent.Data))
 		default:
+			fmt.Printf("%s\n","[+]other msg")
 			continue
 		}
 	}
